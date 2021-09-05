99529fbef46d8ae88529f4dea34b4d30f62b8995
mcartage32 <mcartage33@gmail.com>
2021-09-05 17:33:40 -0500

    Update Entities

---
+++ tallerazure.Functions/Entities/ConsolidatedEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tallerazure.Functions.Entities
+{
+    public class  ConsolidatedEntity : TableEntity
+    {
+        public int EmployedId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int MinutesWork { get; set; }
+    }
+}
